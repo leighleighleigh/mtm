0ed1a5aebea80c4dcf7d73a85749479e6052f5b2
Leigh Oliver <leighcharlesoliver@gmail.com>
2024-02-13 21:32:36 +1100

    Added nix files for local nixpkg use, as well as nix-shell development.

---
+++ shell.nix
@@ -0,0 +1,15 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  mtm = pkgs.callPackage ./default.nix { };
+in
+pkgs.mkShell rec {
+  buildInputs = [ mtm pkgs.ncurses ];
+
+  LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+
+  shellHook = ''
+    export PS1="''${debian_chroot:+($debian_chroot)}\[\033[01;39m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$ "
+    export PS1="(mtm)$PS1"
+  '';
+}
+
